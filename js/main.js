105970b303edca725e82a1a9f734756cdebb97be
SiddharthVarshney <siddharth.98varshney@gmail.com>
2018-12-18 02:01:16 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+function saveBookmark(e){
+    console.log('it works');
+    //prevent form for submitting
+    e.preventDefault();
+
+
+}
