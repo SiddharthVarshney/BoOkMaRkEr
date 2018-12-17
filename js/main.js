447053cc6787534754cf9228c4df1f193076f373
SiddharthVarshney <siddharth.98varshney@gmail.com>
2018-12-18 02:34:19 +0530

    storing form data locally

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 function saveBookmark(e){
-    console.log('it works');
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteUrl').value;
+
+    var bookmark = {
+        name : siteName,
+        url :siteUrl
+    }
+
+    if(localStorage.getItem('bookmarks') === null){
+        var bookmarks = [];
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    }else{
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+
+    }
+
+
     //prevent form for submitting
     e.preventDefault();
+
+
 
 
 }
